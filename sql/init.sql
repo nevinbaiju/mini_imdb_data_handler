02a6d49e4bcca4d6495372cfb27cc2f7f8e88ea0
Nevin Baiju <nevinbaiju@gmail.com>
2024-03-22 20:02:14 -0400

    Add mysql (unstable)

---
+++ sql/init.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS movie_db;
+
+USE movie_db;
+
+CREATE TABLE IF NOT EXISTS avg_ratings (
+    movie_id INT PRIMARY KEY,
+    title VARCHAR(255),
+    avg_rating DECIMAL(2, 2),
+    rating_std DECIMAL(2, 2),
+    count INT
+);
+
+LOAD DATA INFILE '/data/avg_ratings.csv'
+INTO TABLE avg_ratings
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(movie_id, title, avg_rating, rating_std, count);
